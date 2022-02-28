16f01ba2db94d8fecf60be756e2d6e3273b8343e
Laxyny <kevgreg30@gmail.com>
2022-02-28 14:12:04 +0100

    Init

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+           <code></code> Bienvenue
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/Laxyny"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Laxyny
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
